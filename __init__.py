349cf66bed8448b21cdb1f4019db16f58fdfcb87
hana bouzid <hana.bouzid95@gmail.com>
2020-03-13 12:01:15 +0100

    first commit

--- __init__.py
+++ __init__.py
@@ -7,9 +7,9 @@ class MyFirstSkill(MycroftSkill):
     def __init__(self):
         super(MyFirstSkill, self).__init__(name="MyFirstSkill")
 
-    @intent_handler(IntentBuilder("").require("ask"))
+    @intent_handler(IntentBuilder("").require('ask'))
     def handle_hello_world_intent(self, message):
-        self.speak_dialog("rep")
+        self.speak_dialog('rep')
 
 def create_skill():
     return MyFirstSkill()
