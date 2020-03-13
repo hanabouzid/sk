3630d9474c02a5242df51f7f2629f1b5688b1348
hana bouzid <hana.bouzid95@gmail.com>
2020-03-13 14:14:34 +0100

    testing with ghaith

---
+++ README.md
@@ -0,0 +1,28 @@
+## Focus Definition
+Focus definition skill
+
+## Description
+This skill will allow the user to get the definition of focus corporation
+
+## Examples
+ - "Can you tell me about focus"
+ - "Focus corporation"
+ - "Focus"
+ - "What is focus"
+ - "What is focus corporation"
+ - "Tell me about focus"
+ - "Tell me about focus corporation"
+ - "Give me the definition of focus"
+ - "Give me the definition of focus corporation"
+ - "What can you tell me about focus"
+ - "What can you tell me about focus corporation"
+ - "What do you know about focus"
+ - "What do you know about focus corporation ]"
+ - "Focus definition"
+ - "Focus corporation definition"
+
+
+## Credits
+GBHSAZ
+
+
--- __init__.py
+++ __init__.py
@@ -1,15 +1,16 @@
 from adapt.intent import IntentBuilder
-from mycroft.skills.core import MycroftSkill, intent_handler
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill
+from mycroft.skills.core import intent_handler
 
 
-class MyFirstSkill(MycroftSkill):
+class FocusDefinition(MycroftSkill):
     def __init__(self):
-        super(MyFirstSkill, self).__init__(name="MyFirstSkill")
+        MycroftSkill.__init__(self)
 
-    @intent_handler(IntentBuilder("").require('ask'))
-    def handle_hello_world_intent(self, message):
-        self.speak_dialog('rep')
+    @intent_handler(IntentBuilder("").require('hana.focus'))
+    def handle_focus_def(self):
+        self.speak_dialog('hana.focus')
+
 
 def create_skill():
-    return MyFirstSkill()
+    return FocusDefinition()
